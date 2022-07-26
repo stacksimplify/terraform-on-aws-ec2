e5e820c3d3f598bd6b54c38064704489ff05100a
Hemanth Kumar M S <Hemanthhp1229@gmail.com>
2022-07-26 01:38:21 -0500

    Update c1-versions.tf
    changing the terraform version

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -1,18 +1,17 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+    required_versioin = "~>1.2.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "4.23.0"
     }
-  } 
-}  
-# Provider Block
-provider "aws" {
-  region = "us-east-1"
+  }
 }
 
+provider "aws" {
+  region = "us-east-1"
+  
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
 $HOME/.aws/credentials
