159012d660cfa9d885472b75819424bab6db9292
Hemanth Kumar M S <Hemanthhp1229@gmail.com>
2022-07-27 07:48:20 -0500

    Update c6-outputs.tf
    my output values

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c6-outputs.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c6-outputs.tf
@@ -1,13 +1,11 @@
-# Terraform Output Values
-
-# EC2 Instance Public IP
 output "instance_publicip" {
-  description = "EC2 Instance Public IP"
-  value = aws_instance.myec2vm.public_ip
+    description = "ec2 instance public ip"
+    value = aws_instance.myec2vm.public_ip
+  
 }
 
-# EC2 Instance Public DNS
 output "instance_publicdns" {
-  description = "EC2 Instance Public DNS"
-  value = aws_instance.myec2vm.public_dns
+    description = "ec2 instance public ip"
+    value = aws_instance.myec2vm.public_dns
+  
 }
