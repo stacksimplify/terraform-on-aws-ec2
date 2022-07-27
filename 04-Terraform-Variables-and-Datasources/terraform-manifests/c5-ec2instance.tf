a496910d97b0c8d675cf1c4a3b88d9200958cbd0
Hemanth Kumar M S <Hemanthhp1229@gmail.com>
2022-07-27 07:47:43 -0500

    Update c5-ec2instance.tf
    i changed the sg names

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf
@@ -1,11 +1,10 @@
-# EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = data.aws_ami.amzlinux2.id
+  ami = data.aws_ami.amz_linux2.id
   instance_type = var.instance_type
   user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
-  vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id, aws_security_group.allow_web.id ]
   tags = {
-    "Name" = "EC2 Demo 2"
+    "Name" = "MyDemoEC2"
   }
 }
