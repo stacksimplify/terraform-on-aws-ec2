fddf333c8ffc6f38802f44868c2e26cc931a9223
Hemanth Kumar M S <Hemanthhp1229@gmail.com>
2022-07-27 07:45:09 -0500

    Update c2-variables.tf
    my variables

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
@@ -1,23 +1,25 @@
 # Input Variables
 # AWS Region
 variable "aws_region" {
-  description = "Region in which AWS Resources to be created"
+  description = "Region of aws resources"
   type = string
   default = "us-east-1"
-}
 
+}
 # AWS EC2 Instance Type
 variable "instance_type" {
-  description = "EC2 Instnace Type"
+  description = "EC2 instance type"
   type = string
-  default = "t3.micro"
+  default = "t2.micro"
+
+  
 }
 
 # AWS EC2 Instance Key Pair
+
 variable "instance_keypair" {
-  description = "AWS EC2 Key Pair that need to be associated with EC2 Instance"
+  description = "aws keypair to login"
   type = string
-  default = "terraform-key"
+  default = "myterraformkeypair"
+  
 }
-
-
