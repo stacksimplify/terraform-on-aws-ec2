c6d4a255b9fa365fc021acfa0786c90b804f6975
Hemanth Kumar M S <Hemanthhp1229@gmail.com>
2022-08-06 02:13:42 -0500

    playing with maps and list

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c5-ec2instance.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c5-ec2instance.tf
@@ -1,9 +1,9 @@
 # EC2 Instance
 resource "aws_instance" "myec2vm" {
   ami = data.aws_ami.amzlinux2.id
-  instance_type = var.instance_type
+#   instance_type = var.instance_type
   #instance_type = var.instance_type_list[1]  # For List
-  #nstance_type = var.instance_type_map["prod"]  # For Map
+  instance_type = var.instance_type_map["prod"]  # For Map
   user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
   vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
