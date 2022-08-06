1ffb88bb528e252fed26cf4ea548735c24c94eb3
Hemanthkumar Malakavaripallisudharshan <M1051164@granicus.com>
2022-08-06 12:16:42 -0500

    minor changes

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v2-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v2-get-instancetype-supported-per-az-in-a-region.tf
@@ -30,9 +30,9 @@ output "output_v2_1" {
 
 #Output-2
 # Create a Map with Key as Availability Zone and value as Instance Type supported
-# output "output_v2_2" {
-#   value = {
-#     for az, details in data.aws_ec2_instance_type_offerings.my_ins_type2: az => details.instance_types
-#   }
-# }
+output "output_v2_2" {
+  value = {
+  for az, details in data.aws_ec2_instance_type_offerings.my_ins_type2: az => details.instance_types
+}
+}
 
