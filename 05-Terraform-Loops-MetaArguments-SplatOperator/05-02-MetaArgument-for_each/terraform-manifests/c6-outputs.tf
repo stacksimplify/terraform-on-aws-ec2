bab2154a0d6502a0b22cf0fb805c848a0af764cb
Hemanthkumar Malakavaripallisudharshan <M1051164@granicus.com>
2022-08-06 08:11:04 -0500

    changed the output.tf file

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c6-outputs.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c6-outputs.tf
@@ -2,25 +2,12 @@
 
 
 # EC2 Instance Public IP with TOSET
-output "instance_publicip" {
-  description = "EC2 Instance Public IP"
-  #value = aws_instance.myec2vm.*.public_ip   # Legacy Splat
-  #value = aws_instance.myec2vm[*].public_ip  # Latest Splat
-  value = toset([for instance in aws_instance.myec2vm: instance.public_ip])
-}
 
 # EC2 Instance Public DNS with TOSET
-output "instance_publicdns" {
-  description = "EC2 Instance Public DNS"
-  #value = aws_instance.myec2vm[*].public_dns  # Legacy Splat
-  #value = aws_instance.myec2vm[*].public_dns  # Latest Splat
-  value = toset([for instance in aws_instance.myec2vm: instance.public_dns])
-}
+
 
 # EC2 Instance Public DNS with TOMAP
-output "instance_publicdns2" {
-  value = tomap({for az, instance in aws_instance.myec2vm: az => instance.public_dns})
-}
+
 
 
 /*
