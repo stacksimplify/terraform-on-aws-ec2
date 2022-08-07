d113edc34771ac12e26646bcd6d198b798fd033d
Hemanthkumar Malakavaripallisudharshan <M1051164@granicus.com>
2022-08-07 11:21:42 -0500

    changed the v3 version

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v3-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v3-get-instancetype-supported-per-az-in-a-region.tf
@@ -46,11 +46,12 @@ output "output_v3_2" {
 # # Output-3
 # # Filtered Output: with Keys Function - Which gets keys from a Map
 # # This will return the list of availability zones supported for a instance type
-# output "output_v3_3" {
-#   value = keys({
-#     for az, details in data.aws_ec2_instance_type_offerings.my_ins_type: 
-#     az => details.instance_types if length(details.instance_types) != 0 })
-# }
+output "output_v3_3" {
+  value = keys({
+    for az, details in data.aws_ec2_instance_type_offerings.my_ins_type3: 
+    az => details.instance_types if length(details.instance_types) != 0
+  })
+}
 
 
 # # Output-4 (additional learning)
