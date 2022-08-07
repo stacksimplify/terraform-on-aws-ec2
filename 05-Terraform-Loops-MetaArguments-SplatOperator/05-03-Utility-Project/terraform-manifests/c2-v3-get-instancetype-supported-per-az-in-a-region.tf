415f524d9b408a242a838a1e25254860e4555553
Hemanthkumar Malakavaripallisudharshan <M1051164@granicus.com>
2022-08-07 10:10:25 -0500

    changed the v3 version

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v3-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v3-get-instancetype-supported-per-az-in-a-region.tf
@@ -34,28 +34,28 @@ output "output_v3_1" {
   }
 }
 
-# Output-2
-# Filtered Output: Exclude Unsupported Availability Zones
-output "output_v3_2" {
-  value = {
-    for az, details in data.aws_ec2_instance_type_offerings.my_ins_type: 
-    az => details.instance_types if length(details.instance_types) != 0 }
-}
+# # Output-2
+# # Filtered Output: Exclude Unsupported Availability Zones
+# output "output_v3_2" {
+#   value = {
+#     for az, details in data.aws_ec2_instance_type_offerings.my_ins_type: 
+#     az => details.instance_types if length(details.instance_types) != 0 }
+# }
 
-# Output-3
-# Filtered Output: with Keys Function - Which gets keys from a Map
-# This will return the list of availability zones supported for a instance type
-output "output_v3_3" {
-  value = keys({
-    for az, details in data.aws_ec2_instance_type_offerings.my_ins_type: 
-    az => details.instance_types if length(details.instance_types) != 0 })
-}
+# # Output-3
+# # Filtered Output: with Keys Function - Which gets keys from a Map
+# # This will return the list of availability zones supported for a instance type
+# output "output_v3_3" {
+#   value = keys({
+#     for az, details in data.aws_ec2_instance_type_offerings.my_ins_type: 
+#     az => details.instance_types if length(details.instance_types) != 0 })
+# }
 
 
-# Output-4 (additional learning)
-# Filtered Output: As the output is list now, get the first item from list (just for learning)
-output "output_v3_4" {
-  value = keys({
-    for az, details in data.aws_ec2_instance_type_offerings.my_ins_type: 
-    az => details.instance_types if length(details.instance_types) != 0 })[0]
-}
+# # Output-4 (additional learning)
+# # Filtered Output: As the output is list now, get the first item from list (just for learning)
+# output "output_v3_4" {
+#   value = keys({
+#     for az, details in data.aws_ec2_instance_type_offerings.my_ins_type: 
+#     az => details.instance_types if length(details.instance_types) != 0 })[0]
+# }
