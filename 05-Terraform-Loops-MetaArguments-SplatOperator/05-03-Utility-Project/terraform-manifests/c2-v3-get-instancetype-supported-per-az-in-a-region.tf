ebaed085504583af73b43cc4f486ce16ed1ab727
Hemanthkumar Malakavaripallisudharshan <M1051164@granicus.com>
2022-08-07 11:13:22 -0500

    changed the v3 version

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v3-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v3-get-instancetype-supported-per-az-in-a-region.tf
@@ -36,11 +36,12 @@ output "output_v3_1" {
 
 # # Output-2
 # # Filtered Output: Exclude Unsupported Availability Zones
-# output "output_v3_2" {
-#   value = {
-#     for az, details in data.aws_ec2_instance_type_offerings.my_ins_type: 
-#     az => details.instance_types if length(details.instance_types) != 0 }
-# }
+output "output_v3_2" {
+  value = {
+    for az, details in data.aws_ec2_instance_type_offerings.my_ins_type3: 
+    az => details.instance_types if length(details.instance_types) != 0
+  }
+}
 
 # # Output-3
 # # Filtered Output: with Keys Function - Which gets keys from a Map
