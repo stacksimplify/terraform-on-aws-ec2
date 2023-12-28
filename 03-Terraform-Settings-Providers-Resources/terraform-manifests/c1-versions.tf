b2cee21e9f826380d448ad65d67c585eb20189c6
Nishitha1992 <nishitha.v1192@gmail.com>
2023-12-28 12:38:10 +0530

    ami-updation

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -1,13 +1,13 @@
 # Terraform Block
 terraform {
-  required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "5.31.0"
     }
-  } 
-}  
+  }
+}
+ 
 # Provider Block
 provider "aws" {
   region = "us-east-1"
