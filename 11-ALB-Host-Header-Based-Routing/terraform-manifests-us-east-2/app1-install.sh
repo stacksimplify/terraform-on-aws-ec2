7981e8219769c672d5bd2067afa9acc56c368064
Kalyan Reddy Daida <stacksimplify@gmail.com>
2023-12-28 10:32:17 +0530

    Welcome to StackSimplify

--- 11-ALB-Host-Header-Based-Routing/terraform-manifests-us-east-2/app1-install.sh
+++ 11-ALB-Host-Header-Based-Routing/terraform-manifests-us-east-2/app1-install.sh
@@ -7,6 +7,9 @@ sudo service httpd start
 sudo echo '<h1>Welcome to StackSimplify - APP-1</h1>' | sudo tee /var/www/html/index.html
 sudo mkdir /var/www/html/app1
 sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
-sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
-
+#sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+sudo curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
+# AWS Documentation to retrieve EC2 Instance Data
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
 
