79621146ae37fe580fa02e9dcdfceb8eac41dcbe
Kevin <kebbakanyi@hotmail.com>
2024-04-21 10:13:27 +0200

    add changes

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/main.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/main.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
+  ami = "ami-0134dde2b68fe1b07"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/provider.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/provider.tf
@@ -9,7 +9,7 @@ terraform {
 }  
 # Provider Block
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-central-1"
 }
 
 /*
